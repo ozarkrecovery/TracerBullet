4eac5b793425bdcf11abdc8acfb6aec47a094d7a
Robby Gregory <robby.gregory@gmail.com>
2012-03-30 23:01:34 -0500

    corected namespace

--- src/Infrastructure/DependencyResolution/BootStrapper.cs
+++ src/Infrastructure/DependencyResolution/BootStrapper.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace OzarkRecovery.Infrastructure.Infrastructure
+namespace OzarkRecovery.Infrastructure.DependencyResolution
 {
 	public class BootStrapper
 	{
 		public static void RegisterIoC(){
-			ObjectFactory.Initialize(x => { 
+			/*ObjectFactory.Initialize(x => { 
 			
 			
-			});
+			});*/
 		}
 	}
 }
