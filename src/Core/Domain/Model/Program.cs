73ee22c3c0d43832f28c76dff9d46d3383a0a484
Robby Gregory <robby.gregory@gmail.com>
2012-03-30 20:55:29 -0500

    initial checkin

---
+++ src/Core/Domain/Model/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OzarkRecovery.Core.Domain.Model
+{
+	public class Program : Entity
+	{
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public IList<Phase> Phases { get; set; }
+		public Treatment Treatment { get; set; }
+	}
+}
---
+++ src/Infrastructure/DataAccess/Impl/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OzarkRecovery.Core.Domain.Interfaces;
+using System.Linq.Expressions;
+using NHibernate;
+using NHibernate.Linq;
+using OzarkRecovery.Core.Domain.Model;
+
+namespace OzarkRecovery.Infrastructure.DataAccess.Impl
+{
+	public class Repository : IRepository	{
+		
+		private ISession _session;
+		public Repository(ISession session)
+		{
+			_session = session;
+		}
+		
+		public IQueryable<T> Find<T>(Expression<Func<T, bool>> predicate) where T : Entity
+		{
+			return _session.Query<T>().Where(predicate);
+		}
+	}
+}
