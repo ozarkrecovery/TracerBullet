a5d53fd56a06a67738aa3db17cf97a260929a359
unknown <darryl@Trader.(none)>
2012-03-31 13:01:53 -0500

    EF files

---
+++ src/Core/Domain/Model/Counselor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OzarkRecovery.Core.Domain.Model
+{
+    public class Counselor : Entity
+    {
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+    }
+}
