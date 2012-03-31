75210190657ec1b5afd4325800bf5dd76e0c2e9f
Rob Tennyson <rob@robtennyson.us>
2012-03-31 18:28:37 -0500

    Reworked to all Id's in URL and renamed Program to Treatment

---
+++ src/Web/Controllers/SupervisorController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace OzarkRecovery.Web.Controllers
+{
+    [Authorize]
+    public class SupervisorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Supervisor list coming soon");
+        }
+    }
+}
