790f167be0e6035035abb6e4277669be780ee3c4
wa1gon <darryl@wagoner.me>
2012-03-31 16:15:34 -0500

    change config on context

--- src/Infrastructure/DataAccess/ORContext.cs
+++ src/Infrastructure/DataAccess/ORContext.cs
@@ -6,6 +6,8 @@ using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using OzarkRecovery.Core;
 using OzarkRecovery.Core.Domain.Model;
+using System.Data.Entity.ModelConfiguration;
+
 
 namespace OzarkRecovery.Infrastructure.DataAccess
 {
@@ -32,15 +34,58 @@ namespace OzarkRecovery.Infrastructure.DataAccess
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
 
             modelBuilder.Entity<Client>().HasKey(c=>c.Id);
-            modelBuilder.Entity<ClientEvent>().HasKey(c => c.Id);
-            modelBuilder.Entity<ClientEventDesc>().HasKey(c => c.Id);
-            modelBuilder.Entity<Counselor>().HasKey(c => c.Id);
-            modelBuilder.Entity<Phase>().HasKey(p => p.Id);
-            modelBuilder.Entity<Program>().HasKey(p => p.Id);
+            //modelBuilder.Entity<Client>()
+
             modelBuilder.Entity<Treatment>().HasKey(t => t.Id);
+
             //modelBuilder.Entity<Order>().Map(o => o.ToTable("Ord"));
             //modelBuilder.Entity<Order>().HasKey(o => o.OrderId);
             //modelBuilder.Entity<Order>().Property(x => x.OrderId).HasColumnName("OrdID");
+        }
+    }
+    public class ClentConfiguration : EntityTypeConfiguration<Client>
+    {
+        public ClentConfiguration()
+        {
+            HasKey(c => c.Id);
+            Property(p => p.FirstName).HasMaxLength(40);
+            Property(p => p.FirstName).HasMaxLength(30);
+        }
+    }
+    public class CounselorConfiguration : EntityTypeConfiguration<Counselor>
+    {
+        public CounselorConfiguration()
+        {
+            HasKey(c => c.Id);
+            Property(p => p.FullName).HasMaxLength(50);
+        }
+    }
+    public class ClientEventConfiguration : EntityTypeConfiguration<ClientEvent>
+    {
+        public ClientEventConfiguration()
+        {
+            HasKey(c => c.Id);
+        }
+    }
+    public class PhaseConfiguration : EntityTypeConfiguration<Phase>
+    {
+        public PhaseConfiguration()
+        {
+            HasKey(c => c.Id);
+        }
+    }
+    public class ProgramConfiguration : EntityTypeConfiguration<Program>
+    {
+        public ProgramConfiguration()
+        {
+            HasKey(c => c.Id);
+        }
+    }
+    public class TreatmentConfiguration : EntityTypeConfiguration<Treatment>
+    {
+        public TreatmentConfiguration()
+        {
+            HasKey(c => c.Id);
         }
     }
 }
