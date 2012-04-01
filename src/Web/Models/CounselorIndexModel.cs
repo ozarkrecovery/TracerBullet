a8692cd108721ab1ab2b50aecdfd308b17dd60e9
Robby Gregory <robby.gregory@gmail.com>
2012-03-31 23:40:33 -0500

    updating

---
+++ src/Web/Models/CounselorIndexModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OzarkRecovery.Core.Domain.Model;
+
+namespace OzarkRecovery.Web.Models
+{
+    public class CounselorIndexModel
+    {
+        public Counselor User { get; private set; }
+        public IList<Counselor> Counselors { get; private set; }
+
+        public CounselorIndexModel(Counselor user, IList<Counselor> counselors)
+        {
+            User = user;
+            Counselors = counselors;
+        }
+    }
+}
