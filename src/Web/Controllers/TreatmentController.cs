2fa94985d551b420bb4a1b999e8c49f20cd5911c
Robby Gregory <robby.gregory@gmail.com>
2012-04-01 10:59:15 -0500

    adding treatment flow

--- src/Web/Controllers/TreatmentController.cs
+++ src/Web/Controllers/TreatmentController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using OzarkRecovery.Core.Domain.Interfaces;
 using OzarkRecovery.Core.Domain.Model;
@@ -19,9 +20,11 @@ namespace OzarkRecovery.Web.Controllers
 
         public ActionResult Show(int clientId, int treatmentNumber)
         {
-            var treatment =
-                _repository.Get<Client>(x => x.Id == clientId).Treatments.OrderBy(y => y.StartDate)[treatmentNumber - 1];
-            return Content(string.Format("Detail view of treatment coming soon... ({0}, {1})", clientId, treatmentNumber));
+            var treatment = _repository.Get<Client>(x => x.Id == clientId)
+                .Treatments
+                .OrderBy(x => x.StartDate)
+                .ElementAt(treatmentNumber - 1);
+            return View(treatment);
         }
     }
 }
