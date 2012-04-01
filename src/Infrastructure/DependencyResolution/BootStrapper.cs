e8f49daa16d0dd706901d0185a094c41cf5866f7
Robby Gregory <robby.gregory@gmail.com>
2012-03-31 20:38:40 -0500

    adding treatment strategy

--- src/Infrastructure/DependencyResolution/BootStrapper.cs
+++ src/Infrastructure/DependencyResolution/BootStrapper.cs
@@ -10,21 +10,50 @@ using System.Data.Entity;
 using OzarkRecovery.Infrastructure.DataAccess;
 using OzarkRecovery.Core.Services;
 using OzarkRecovery.Infrastructure.Services;
+using OzarkRecovery.Core.Domain.Model;
 
 namespace OzarkRecovery.Infrastructure.DependencyResolution
 {
-	public class BootStrapper
-	{
-		public static void RegisterIoC()
-		{
-            Database.SetInitializer<ORContext>(new DropCreateDatabaseIfModelChanges<ORContext>());
-			ObjectFactory.Initialize(x =>
-			{
-				x.For<IRepository>().Use<Repository>();
-			    x.For<DbContext>().Use<ORContext>();
+    public class BootStrapper
+    {
+        public static void RegisterIoC()
+        {
+            Database.SetInitializer<ORContext>(new ORContextInitializer());
+            ObjectFactory.Initialize(x =>
+            {
+                x.For<IRepository>().Use<Repository>();
+                x.For<DbContext>().Use<ORContext>();
                 x.For<ISecurityContextService>().Use<SecurityContextService>();
-			});
-		}
-	}
+            });
+        }
+    }
+
+    public class ORContextInitializer : DropCreateDatabaseAlways<ORContext>
+    {
+        protected override void Seed(ORContext context)
+        {
+            context.Counselor.Add(new Counselor
+            {
+                FirstName = "Joe",
+                LastName = "Super",
+                UserName = "admin",
+                Password = "admin",
+                IsSupervisor = true,
+                IsActive = true
+            });
+            context.Counselor.Add(new Counselor
+            {
+                FirstName = "Jane",
+                LastName = "Doe",
+                UserName = "jane",
+                Password = "doe",
+                IsSupervisor = false,
+                IsActive = true
+            });
+            context.SaveChanges();
+            base.Seed(context);
+        }
+    }
 }
+
 
