035a14cc4d38ff7916e56d3c3746dce3198f82e6
Rob Tennyson <rob@robtennyson.us>
2012-04-01 13:01:41 -0500

    Seed data

--- src/Core/Domain/Model/Step.cs
+++ src/Core/Domain/Model/Step.cs
@@ -1,28 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace OzarkRecovery.Core.Domain.Model
 {
     public class Step : Entity
     {
-        public static Step Unassigned = new Step { Name = "Unassigned" };
-
-        public short Sequence { get; set; }
-        public string Name { get; set; }
-        public IList<Document> Documents { get; set; }
-        public IList<Phase> Phases { get; set; }
-        public IList<Survey> Surveys { get; set; }
-        public bool IsActive { get; set; }
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
+        public static Step Unassigned = new Step {Name = "Unassigned"};
 
         public Step()
         {
             Documents = new List<Document>();
             Phases = new List<Phase>();
             Surveys = new List<Survey>();
+        }
+
+        public short Sequence { get; set; }
+        public string Name { get; set; }
+        public virtual IList<Document> Documents { get; set; }
+        public virtual IList<Phase> Phases { get; set; }
+        public virtual IList<Survey> Surveys { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public void Begin()
+        {
+            StartDate = DateTime.Now;
+            IsActive = true;
+        }
+
+        public void End()
+        {
+            EndDate = DateTime.Now;
+            IsActive = false;
         }
     }
 }
